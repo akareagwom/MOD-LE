8d15548573a47e48fdbc2ce95455faffa8b450ae
akareagwom <akareazikang20@gmail.com>
2024-09-16 13:56:19 +0100

    custom css

--- src/home/components/NavBar.tsx
+++ src/home/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../index.css'
 
 interface Props {
   
@@ -6,9 +7,16 @@ interface Props {
 
 const NavBar: React.FC<Props> = ({  }) => {
   return (
-    <div className='teal '>
-      <h1 className='white-text'>Euphoria</h1>
+    <nav className='brown lighten-3 nav'>
+    <div className="">
+      <a href="#" className="brand-logo">Logo</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="sass.html">HOME</a></li>
+        <li><a href="badges.html">ABOUT</a></li>
+        <li><a href="collapsible.html">CONTACT</a></li>
+      </ul>
     </div>
+  </nav>
   );
 };
 
--- src/home/index.tsx
+++ src/home/index.tsx
@@ -7,9 +7,15 @@ interface Props {
 
 const Home: React.FC<Props> = ({  }) => {
   return (
-    <div>
-      <NavBar/>
-      please,please
+    <div className='black '>
+      
+    <div className="row">
+      <div className="col s12">
+        <NavBar/>
+      </div>
+      <div className="col teal s6">6-columns (one-half)</div>
+      <div className="col teal s6">6-columns (one-half)</div>
+    </div>
     </div>
   );
 };
