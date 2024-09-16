18049824fa110eee61efe1c5fc70fb2dc4f354ea
akareagwom <akareazikang20@gmail.com>
2024-09-16 09:22:39 +0100

    nav

--- src/home/components/NavBar.tsx
+++ src/home/components/NavBar.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Menu, Segment } from 'semantic-ui-react';
 
 interface Props {
   
@@ -7,14 +6,9 @@ interface Props {
 
 const NavBar: React.FC<Props> = ({  }) => {
   return (
-    <Segment  inverted attached size='mini'>
-      <Menu inverted secondary>
-        <Menu.Item>Home</Menu.Item>
-        <Menu.Item>Home</Menu.Item>
-        <Menu.Item>Home</Menu.Item>
-        <Menu.Item>Home</Menu.Item>
-      </Menu>
-    </Segment>
+    <div>
+      hola
+    </div>
   );
 };
 
