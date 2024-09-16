580f2242b695f7f19905bfd088f209b0d67bc1d3
akareagwom <akareazikang20@gmail.com>
2024-09-16 22:38:22 +0100

    grid system

--- src/home/components/AboutCard.tsx
+++ src/home/components/AboutCard.tsx
@@ -8,15 +8,11 @@ const AboutCard: React.FC<Props> = ({  }) => {
   return (
     <div className="row">
     <div className="m6">
-      <div className="card blue-grey darken-1">
+      <div className="card-panel brown darken-1">
         <div className="card-content white-text">
           <span className="card-title">Card Title</span>
           <p>I am a very simple card. I am good at containing small bits of information.
           I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div className="card-action">
-          <a href="#">This is a link</a>
-          <a href="#">This is a link</a>
         </div>
       </div>
     </div>
--- src/home/index.tsx
+++ src/home/index.tsx
@@ -10,14 +10,18 @@ const Home: React.FC<Props> = ({  }) => {
   return (
     <div className='black'>
       
-    <div className="row">
+      <div className="row">
       <div className="col s12">
         <NavBar/>
       </div>
-      <div className="col s5"><AboutCard/></div>
-      <div className="col s3"><AboutCard/></div>
-      <div className="col s4"><AboutCard/></div>
-      <div className="col s5"><AboutCard/></div>
+      <div className="grid-container">
+        <div className="col s12 m4 grid-system l5"><AboutCard/></div>
+        <div className="col s12 m4 grid-system l3"><AboutCard/></div>
+        <div className="col s12 m4 grid-system l3"><AboutCard/></div>
+      </div>
+      {/* <div className="col s4 push-s0"><span className="flow-text red-text"><AboutCard/></span></div>
+      <div className="col s4 push-s0"><span className="flow-text red-text"><AboutCard/></span></div>
+      <div className="col s4 push-s0"><span className="flow-text red-text"><AboutCard/></span></div> */}
     </div>
     </div>
   );
