b96dfc8e28f31d0be06ae3702235722cefe93828
akareagwom <akareazikang20@gmail.com>
2024-09-18 16:46:30 +0100

    main card image

--- src/home/components/MainCard.tsx
+++ src/home/components/MainCard.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 const MainCard: React.FC<Props> = ({  }) => {
   return (
-    <div className='bg-orange-200'>
-      <div className="">
+    <div className='bg-orange-200 rounded-xl'>
+      <div className="p-6">
         <img className='rounded-xl' src="model.avif" alt="" />
       </div>
     </div>
