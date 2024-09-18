c2175af27a86299044febe63e5517ea105aeb7dd
akareagwom <akareazikang20@gmail.com>
2024-09-18 20:54:28 +0100

    main card content

--- src/home/components/MainCard.tsx
+++ src/home/components/MainCard.tsx
@@ -6,14 +6,18 @@ interface Props {
 
 const MainCard: React.FC<Props> = ({  }) => {
   return (
-    <div className='bg-[#cbb89d] w-[70%] rounded-xl'>
-      <h1 className='text-white text-2xl'>staedy</h1>
+    <div className='bg-[#cbb89d] w-[60%] rounded-xl'>
       <div className="py-4 px-4">
-        <img className='rounded-xl' src="model.avif" alt="" />
+        <h1 className='text-white text-2xl align-start'>steady</h1>
+        <img className='rounded-xl my-2' src="model.avif" alt="" />
 
-      </div>
-      <div className="">
-
+        <div className="">
+          <ul>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
