1def214e7f94f77f316dfc8873d834ef635afc0e
akareagwom <akareazikang20@gmail.com>
2024-09-23 22:00:43 +0100

    image slide

--- src/home/components/PhotoSlider.tsx
+++ src/home/components/PhotoSlider.tsx
@@ -21,9 +21,15 @@ const PhotoSlider: React.FC<Props> = ({  }) => {
         totalSlides={3}
       >
         <Slider className='text-white'>
-          <Slide  index={0}>I am the first Slide.</Slide>
-          <Slide index={1}>I am the second Slide.</Slide>
-          <Slide index={2}>I am the third Slide.</Slide>
+          <Slide  index={0}>
+             <img className='grayscale rounded-xl w-[250px] h-[250px]' src="https://plus.unsplash.com/premium_photo-1724017205222-45a29f1cacd4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="photo" />
+          </Slide>
+          <Slide index={1}>
+             <img className='grayscale rounded-xl w-[250px] h-[250px]' src="https://plus.unsplash.com/premium_photo-1724017205222-45a29f1cacd4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="photo" />
+          </Slide>
+          <Slide index={2}>
+             <img className='grayscale rounded-xl w-[250px] h-[250px]' src="https://plus.unsplash.com/premium_photo-1724017205222-45a29f1cacd4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="photo" />
+          </Slide>
         </Slider>
         <ButtonBack className='text-white'>Back</ButtonBack>
         <ButtonNext className='text-white'>Next</ButtonNext>
